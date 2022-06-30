700a9c46cefd70646063a8a70ba19baf75b71426
Thibault Touzin <thibault.touzin@gmail.com>
2022-06-30 15:40:12 +0200

    adding first twig

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -1,16 +1,18 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller;//"chemin" pour récup nos classes
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;//héritage
+use Symfony\Component\HttpFoundation\Response;//pour que ça marche lol
+use Symfony\Component\Routing\Annotation\Route;//pour rediriger url
 
 class AccueilController extends AbstractController {
     /**
      * @Route("/",name="accueil");
      */
     public function accueil(){
-        return new Response("Bonjour sans twig :p");
+        $name = 'thibault';
+        return $this->render('learn.html.twig',//on peut retourner this grâce à l'héritage
+        ['name'=>$name]);//on place la variable à l'index name du tableau
     }
 }
