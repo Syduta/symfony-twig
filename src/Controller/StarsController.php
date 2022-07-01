2136e143e2b4bc60246c0ccb067f91e89ce2a6c1
Thibault Touzin <thibault.touzin@gmail.com>
2022-07-01 16:11:18 +0200

    link for details on each star

--- src/Controller/StarsController.php
+++ src/Controller/StarsController.php
@@ -19,9 +19,10 @@ class StarsController extends AbstractController{
         'title' => 'Tex',
          'description' => 'mauvaise blague',
         'color' => 'red',
-        'published' => false,
+        'published' => true,
         'image' => 'https://resize.programme-television.ladmedia.fr/r/670,670/img/var/premiere/storage/images/tele-7-jours/news-tv/video-les-z-amours-france-2-tex-craque-sa-chemise-devant-les-candidats-4344637/77880010-1-fre-FR/VIDEO-Les-Z-amours-France-2-Tex-craque-sa-chemise-devant-les-candidats.jpg',
-        'publishedDate' => new \DateTime('NOW')
+        'publishedDate' => new \DateTime('NOW'),
+        'id'=>1
         ],
         2 => [
         'title' => 'Cyril Hanouna',
@@ -29,7 +30,8 @@ class StarsController extends AbstractController{
         'color' => 'yellow',
         'published' => true,
         'image' => 'https://media.onvoitout.fr/2021/10/cyril-hanouna-tpmp-stresse-tres-inquiet-pour-les-yeux-de-benjamin-castaldi-il-dit-tout-50.jpg',
-        'publishedDate' => new \DateTime('NOW')
+        'publishedDate' => new \DateTime('NOW'),
+         'id'=>2
         ],
         3 => [
         'title' => 'Afida Turner',
@@ -37,23 +39,26 @@ class StarsController extends AbstractController{
         'color' => 'pink',
         'published' => true,
         'image' => 'https://s.yimg.com/uu/api/res/1.2/SXrg2uonJIrD9JlemslJWQ--~B/aD01Njg7dz0xMDI0O2FwcGlkPXl0YWNoeW9u/http://media.zenfs.com/fr_FR/News/TeleLoisirs/96851-afida-turner-atteinte-de-deux-tumeurs-elle-sera-bientot-hospitalisee.jpeg',
-        'publishedDate' => new \DateTime('NOW')
+        'publishedDate' => new \DateTime('NOW'),
+        'id'=>3
         ],
         4 => [
         'title' => 'Manuel Valls',
         'description' => 'Eric',
         'color' => 'blue',
-        'published' => false,
+        'published' => true,
         'image' => 'https://images.bfmtv.com/vypLwZsN4IyBgQiA4BpWRJgRrpg=/4x3:1252x705/1248x0/images/-80516.jpg',
-        'publishedDate' => new \DateTime('NOW')
+        'publishedDate' => new \DateTime('NOW'),
+         'id'=>4
         ],
         5 => [
         'title' => 'JCVD',
         'description' => 'Eric',
         'color' => 'blue',
-        'published' => false,
+        'published' => true,
         'image' => 'https://www.programme.tv/imgre/fit/~1~tel~2021~07~12~6801abcb-554d-4a8b-8667-d05cb7f88734.png/1160x500/crop-from/top/quality/80/focus-point/1783,871/jean-claude-van-damme-devoile-les-causes-de-ses-problemes-de-drogue.jpg',
-        'publishedDate' => new \DateTime('NOW')
+        'publishedDate' => new \DateTime('NOW'),
+        'id'=>5
         ]
     ];
 //    dd($stars);
@@ -76,9 +81,10 @@ class StarsController extends AbstractController{
                     'title' => 'Tex',
                     'description' => 'mauvaise blague',
                     'color' => 'red',
-                    'published' => false,
+                    'published' => true,
                     'image' => 'https://resize.programme-television.ladmedia.fr/r/670,670/img/var/premiere/storage/images/tele-7-jours/news-tv/video-les-z-amours-france-2-tex-craque-sa-chemise-devant-les-candidats-4344637/77880010-1-fre-FR/VIDEO-Les-Z-amours-France-2-Tex-craque-sa-chemise-devant-les-candidats.jpg',
-                    'publishedDate' => new \DateTime('NOW')
+                    'publishedDate' => new \DateTime('NOW'),
+                    'id'=>1
                 ],
                 2 => [
                     'title' => 'Cyril Hanouna',
@@ -86,7 +92,8 @@ class StarsController extends AbstractController{
                     'color' => 'yellow',
                     'published' => true,
                     'image' => 'https://media.onvoitout.fr/2021/10/cyril-hanouna-tpmp-stresse-tres-inquiet-pour-les-yeux-de-benjamin-castaldi-il-dit-tout-50.jpg',
-                    'publishedDate' => new \DateTime('NOW')
+                    'publishedDate' => new \DateTime('NOW'),
+                    'id'=>2
                 ],
                 3 => [
                     'title' => 'Afida Turner',
@@ -94,23 +101,26 @@ class StarsController extends AbstractController{
                     'color' => 'pink',
                     'published' => true,
                     'image' => 'https://s.yimg.com/uu/api/res/1.2/SXrg2uonJIrD9JlemslJWQ--~B/aD01Njg7dz0xMDI0O2FwcGlkPXl0YWNoeW9u/http://media.zenfs.com/fr_FR/News/TeleLoisirs/96851-afida-turner-atteinte-de-deux-tumeurs-elle-sera-bientot-hospitalisee.jpeg',
-                    'publishedDate' => new \DateTime('NOW')
+                    'publishedDate' => new \DateTime('NOW'),
+                    'id'=>3
                 ],
                 4 => [
                     'title' => 'Manuel Valls',
                     'description' => 'Eric',
                     'color' => 'blue',
-                    'published' => false,
+                    'published' => true,
                     'image' => 'https://images.bfmtv.com/vypLwZsN4IyBgQiA4BpWRJgRrpg=/4x3:1252x705/1248x0/images/-80516.jpg',
-                    'publishedDate' => new \DateTime('NOW')
+                    'publishedDate' => new \DateTime('NOW'),
+                    'id'=>4
                 ],
                 5 => [
                     'title' => 'JCVD',
                     'description' => 'Eric',
                     'color' => 'blue',
-                    'published' => false,
+                    'published' => true,
                     'image' => 'https://www.programme.tv/imgre/fit/~1~tel~2021~07~12~6801abcb-554d-4a8b-8667-d05cb7f88734.png/1160x500/crop-from/top/quality/80/focus-point/1783,871/jean-claude-van-damme-devoile-les-causes-de-ses-problemes-de-drogue.jpg',
-                    'publishedDate' => new \DateTime('NOW')
+                    'publishedDate' => new \DateTime('NOW'),
+                    'id'=>5
                 ]
             ];
         return $this->render('star.html.twig',
@@ -118,4 +128,18 @@ class StarsController extends AbstractController{
     }
 
 }
+
+
+//{#
+//    je veux générer une url vers la page d'affichage d'une star
+//                j'évite de mettre l'url en dur, sinon l'url ne marchera
+//                sur les environements autres que le mien (autres dev, serveur de prod etc)
+//                Pour calculer l'url en fonction de l'environnement, j'utilise la
+//                fonction path de twig et je lui passe juste le nom de la route.
+//Le problème de cette route (star) c'est qu'elle n'est pas statique. On est
+//                obligé de lui passer un id, pour identifier la star à afficher.
+//                Donc en deuxième parametre, on lui passe l'id à utiliser dans la route,
+//                ici l'id de la star.
+//            #}
+//            <a href={{ path('star', {'id': star.id }) }}>Voir le détail de la star</a>
 
